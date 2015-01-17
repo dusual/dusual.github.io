7a2c0bea6776becb7ecabe43b39a38d7de6fb216
Amit Singh Sethi <amit.pureenergy@gmail.com>
2015-01-17 22:28:16 +0530

    conf changes

--- conf.py
+++ conf.py
@@ -200,12 +200,16 @@ TIMEZONE = "Asia/Kolkata"
 #
 
 POSTS = (
-    ("posts/*.rst", "posts", "post.tmpl"),
+    ("posts/*.md", "posts", "post.tmpl"),
+    ("posts/*.ipynb", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
+    ("posts/*.rst", "posts", "post.tmpl"),
 )
 PAGES = (
-    ("stories/*.rst", "stories", "story.tmpl"),
+    ("stories/*.md", "stories", "story.tmpl"),
+    ("stories/*.ipynb", "stories", "story.tmpl"),
     ("stories/*.txt", "stories", "story.tmpl"),
+    ("stories/*.rst", "stories", "story.tmpl"),
 )
 
 # One or more folders containing files to be copied as-is into the output.
